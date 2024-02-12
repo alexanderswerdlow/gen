Fix quaternion order

gen/datasets/kubrics/movi_dataset.py
@@ -10,6 +10,7 @@ import torch
 import torchvision
 import webdataset as wds
 from einops import rearrange
+from einx import roll
 from ipdb import set_trace as st
 from torch.utils.data import Dataset
 from scipy.spatial.transform import Rotation as R
@@ -112,14 +113,19 @@ class MoviDataset(AbstractDataset, Dataset):
             instance = data["segment"][camera_idx, frame_idx]
 
             quaternions = data["quaternions"][camera_idx, frame_idx] # (23, 4)
+            quaternions = roll('objects [wxyz]', quaternions, shift=(-1,))
             positions = data["positions"][camera_idx, frame_idx] # (23, 3)
             valid = data["valid"][camera_idx, :].squeeze(0) # (23, )
             categories = data["categories"][camera_idx, :].squeeze(0) # (23, )
 
             if 'camera_quaternions' in data:
                 camera_quaternion = data['camera_quaternions'][camera_idx, frame_idx] # (4, )
+                camera_quaternion = roll('[wxyz]', camera_quaternion, shift=(-1,))
                 quaternions[~valid] = 1 # Set invalid quaternions to 1 to avoid 0 norm.
-                quaternions = (R.from_quat(quaternions) * R.from_quat(camera_quaternion).inv()).as_quat()
+                quaternions = (
+                    R.from_quat(quaternions)
+                    * R.from_quat(camera_quaternion).inv()
+                ).as_quat()
                 quaternions[~valid] = 0
             else:
                 raise NotImplementedError("Camera quaternions not found in data.npz")

gen/models/cross_attn/base_inference.py
@@ -110,7 +110,7 @@ def run_quantitative_inference(self: BaseMapper, batch: dict, state: TrainingSta
             pred_data.pred_6d_rot = pred_data.pred_6d_rot[pred_data.pred_mask]
             pred_data.gt_rot_6d = pred_data.gt_rot_6d[pred_data.pred_mask]
             pred_loss = F.mse_loss(pred_data.pred_6d_rot, pred_data.gt_rot_6d, reduction='none')
-            ret['pred_loss'] = pred_loss.float().cpu()
+            ret['rot_pred_loss'] = pred_loss.float().cpu()
 
     if self.cfg.model.token_cls_pred_loss:
         loss_ret = token_cls_loss(self.cfg, batch, cond, pred_data)
@@ -174,7 +174,6 @@ def run_qualitative_inference(self: BaseMapper, batch: dict, state: TrainingStat
             ret["rotations"] = Im.concat_vertical(*all_imgs, spacing=30, fill=(128, 128, 128))
             
     if self.cfg.model.unet is False: return ret
-
     
     gt_info = Im.concat_vertical(orig_image, get_layered_image_from_binary_mask(batch["gen_segmentation"].squeeze(0))).write_text(text="GT")
     ret["validation"] = gt_info

gen/models/cross_attn/losses.py
@@ -193,9 +193,9 @@ def token_cls_loss(
     top5_accuracy = (correct_top5_predictions / total_instances) if total_instances > 0 else 0
 
     return {
-        "token_cls_pred_loss": avg_loss * 0.1,
-        "metric_token_cls_pred_acc": torch.tensor(accuracy, device=device),
-        "metric_token_cls_pred_top5_acc": torch.tensor(top5_accuracy, device=device),
+        "cls_pred_loss": avg_loss * 0.1,
+        "metric_cls_pred_acc": torch.tensor(accuracy, device=device),
+        "metric_cls_pred_top5_acc": torch.tensor(top5_accuracy, device=device),
     }
 
 
@@ -228,4 +228,4 @@ def token_rot_loss(cfg: BaseConfig, batch: InputData, cond: ConditioningData, pr
     else:
         loss = F.mse_loss(pred_data.pred_6d_rot[pred_data.pred_mask], pred_data.gt_rot_6d[pred_data.pred_mask], reduction="mean")
 
-    return {"token_rot_pred_loss": loss}
+    return {"rot_pred_loss": loss}